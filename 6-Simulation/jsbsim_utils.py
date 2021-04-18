4c0843af7a6b3d382d8dfd70fd54b3b4fd63adff
James Goppert <james.goppert@gmail.com>
2021-04-17 22:33:53 -0400

    Update trim notebook.

--- 6-Simulation/jsbsim_utils.py
+++ 6-Simulation/jsbsim_utils.py
@@ -34,15 +34,18 @@ class Logger:
     
 def set_location_purdue_airport(fdm):
     # location, Purdue airport
-    fdm['ic/terrain-elevation-ft'] = 584.5
+    fdm['ic/terrain-elevation-ft'] = 586
     fdm['ic/lat-geod-deg'] = 40.4110213580397
     fdm['ic/long-gc-deg'] = -86.92756398413263
     fdm['ic/psi-true-deg'] = 280
 
 
-def trim(aircraft, ic, design_vector, x0, verbose, cost=None, **kwargs):
+def trim(aircraft, ic, design_vector, x0, verbose,
+         cost=None, eq_constraints=None, tol=1e-6, ftol=None, show=False, **kwargs):
     root = Path('.').resolve()
     fdm = jsbsim.FGFDMExec(str(root)) # The path supplied to FGFDMExec is the location of the folders "aircraft", "engines" and "systems"
+    if show:
+        fdm.set_output_directive(str(root/ 'data_output' / 'flightgear.xml'))
     fdm.set_debug_level(0)    
     fdm.load_model(aircraft)
 
@@ -57,16 +60,19 @@ def trim(aircraft, ic, design_vector, x0, verbose, cost=None, **kwargs):
     if cost is None:
         def cost(fdm):
             # compute cost, force moment balance
+            theta = fdm['attitude/theta-rad']
             udot = fdm['accelerations/udot-ft_sec2']
             vdot = fdm['accelerations/vdot-ft_sec2']
             wdot = fdm['accelerations/wdot-ft_sec2']
             pdot = fdm['accelerations/pdot-rad_sec2']
             qdot = fdm['accelerations/qdot-rad_sec2']
             rdot = fdm['accelerations/rdot-rad_sec2']
-            return udot**2 + vdot**2 + wdot**2 + pdot**2 + qdot**2 + rdot**2
+            # (theta < 0) term prevents nose down trim
+            return udot**2 + vdot**2 + wdot**2 + \
+                pdot**2 + qdot**2 + rdot**2 + + 1e-3*(theta < 0)
 
     # cost function for trimming
-    def trim_cost(fdm, xd):
+    def eval_fdm_func(xd, fdm, ic, fdm_func):
         # set initial condition
         for var in ic.keys():
             fdm[var] = ic[var]
@@ -82,23 +88,47 @@ def trim(aircraft, ic, design_vector, x0, verbose, cost=None, **kwargs):
         # set initial conditions
         fdm.run_ic()
         
-        return cost(fdm)
+        return fdm_func(fdm)
+
+    # setup constraints
+    constraints = []
+    if eq_constraints is None:
+        eq_constraints = []
+    for con in eq_constraints:
+        constraints.append({
+            'type': 'eq',
+            'fun': eval_fdm_func,
+            'args': (fdm, ic, con)
+        })
     
-    res = scipy.optimize.minimize(fun=lambda xd: trim_cost(fdm, xd), x0=x0, **kwargs)
+    # solve
+    res = scipy.optimize.minimize(
+        fun=eval_fdm_func,
+        args=(fdm, ic, cost), x0=x0, 
+        constraints=constraints, **kwargs)
+    
     if verbose:
         print(res)
+    
+    # update ic
     for i, var in enumerate(design_vector):
         ic[var] = res['x'][i]
     
-    return ic, fdm
+    if verbose:
+        for con in constraints:
+            print('constraint', con['type'], con['fun'](res['x'], *con['args']))
+    
+    if not res['success'] or ftol is not None and abs(res['fun']) > ftol:
+        raise RuntimeError('trim failed:\n' + str(res) + '\n' + \
+                           str(fdm.get_property_catalog('acceleration')))
+    props = fdm.get_property_catalog('')
+    del fdm
+    return ic, props
 
 
-def simulate(aircraft, op_0, op_list=None, op_times=None, tf=50, realtime=False):
+def simulate(aircraft, op_0, op_list=None, tf=50, realtime=False, verbose=False):
     if op_list is None:
         op_list = []
-    
-    if op_times is None:
-        op_times = []
         
     root = Path('.').resolve()
     fdm = jsbsim.FGFDMExec(str(root)) # The path supplied to FGFDMExec is the location of the folders "aircraft", "engines" and "systems"
@@ -135,13 +165,19 @@ def simulate(aircraft, op_0, op_list=None, op_times=None, tf=50, realtime=False)
     frame_duration = fdm.get_delta_t()
     op_count = 0
     result = fdm.run()
-
+    low_fuel_warned = False
+    
     while result and fdm.get_sim_time() < tf:
         t = fdm.get_sim_time()
-        if op_count < len(op_times) and t > op_times[op_count]:
-            set_opereating_point(op_list[op_count])
+        if op_count < len(op_list) and op_list[op_count][2](fdm):
+            if verbose:
+                print('operating point reached: ', op_list[op_count][0])
+            set_opereating_point(op_list[op_count][1])
             op_count += 1
-
+        
+        # rough approximation of grade of runway 28 at purdue airport
+        if fdm['position/distance-from-start-mag-mt'] < 2000:
+            fdm['position/terrain-elevation-asl-ft'] = 586 + 0.02*fdm['position/distance-from-start-mag-mt']
         if realtime:
             current_seconds = time.time()
             actual_elapsed_time = current_seconds - initial_seconds
@@ -154,10 +190,15 @@ def simulate(aircraft, op_0, op_list=None, op_times=None, tf=50, realtime=False)
             result = fdm.run()
             if nice:
                 time.sleep(sleep_nseconds / 1000000.0)
-        log.new_frame(
-            t, fdm.get_property_catalog(''))
+        
+        if not low_fuel_warned and fdm['propulsion/total-fuel-lbs'] < 10:
+            print('warning: LOW FUEL {:0.2f} lbs, restart simulation'.format(fdm['propulsion/total-fuel-lbs']))
+            low_fuel_warned = True
+
+        log.new_frame(t, fdm.get_property_catalog(''))
 
     log = log.to_pandas()
+    del fdm
     return log
 
 
@@ -223,6 +264,7 @@ def linearize(aircraft, states, states_deriv, inputs, outputs, ic, dx, n_round=3
         for j, output in enumerate(outputs):
             D[j, i] = (fdm[output] - start[output]) / dx
     
+    del fdm
     A = np.round(A, n_round)
     B = np.round(B, n_round)
     C = np.round(C, n_round)
